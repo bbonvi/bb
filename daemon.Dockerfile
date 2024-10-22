fe9e29f1d48f8763a698205bab3c91f8671cadc0
Bonanov <6573230@gmail.com>
2024-10-22 18:24:50 +0300

    build: docker

---
+++ daemon.Dockerfile
@@ -0,0 +1,62 @@
+FROM node AS node_builder 
+WORKDIR /app
+COPY client/package.json client/yarn.lock ./
+RUN yarn
+COPY client/ ./
+RUN yarn run build
+
+#########################
+
+FROM rust:alpine AS rust_builder 
+WORKDIR /app
+
+RUN apk add --no-cache openssl-dev musl-dev openssl-libs-static
+
+RUN rustup toolchain list | head -1 | cut -d" " -f1 | sed -e "s/gnu/musl/" > /target.txt
+
+RUN rustup target add $(cat /target.txt | cut -d"-" -f2- )
+
+COPY src src
+COPY Cargo.lock Cargo.toml .
+
+ARG NO_HEADLESS="false"
+
+ENV CFLAGS="-fPIC" 
+
+RUN if [ "$NO_HEADLESS" = "true" ]; then \
+        echo "building without chromium" && \
+        cargo install --no-default-features \
+                --target=$(cat /target.txt | cut -d"-" -f2-) \
+                --root /usr/local/ --path ./ ; \
+    else \
+        echo "building with chromium" && \
+        cargo install --target=$(cat /target.txt | cut -d"-" -f2-) --root /usr/local/ --path ./ ; \
+    fi
+
+#########################
+
+FROM alpine:latest
+
+ARG NO_HEADLESS="false"
+
+RUN if [ "$NO_HEADLESS" != "true" ]; then \
+        apk add --no-cache \
+            chromium \
+            gcompat \
+            harfbuzz \
+            nss \
+            freetype \
+            ttf-freefont \
+            && mkdir /var/cache/chromium \
+            && chown root:root /var/cache/chromium; \
+    fi
+
+COPY --from=node_builder /app/build /client/build
+COPY --from=rust_builder /usr/local/bin/bb /usr/local/bin/bb
+
+ENV RUST_LOG="info,bb=info,tower_http::trace::on_response=warn"
+
+EXPOSE 8080
+
+CMD ["/usr/local/bin/bb", "daemon"]
+
---
+++ src/scrape/headless.rs
@@ -0,0 +1,192 @@
+use headless_chrome::{
+    protocol::cdp::{Page, Target::CreateTarget},
+    LaunchOptionsBuilder, Tab,
+};
+use std::{path::PathBuf, str::FromStr, sync::Arc, thread::sleep, time::Duration};
+
+pub struct ChromeResult {
+    pub html: String,
+    pub screenshot: Vec<u8>,
+}
+
+pub fn stealth_tab(tab: Arc<Tab>) {
+    tab.call_method(Page::AddScriptToEvaluateOnNewDocument {
+        source: "Object.defineProperty(navigator, 'webdriver', {get: () => undefined});"
+            .to_string(),
+        world_name: None,
+        include_command_line_api: None,
+    })
+    .unwrap();
+    tab.set_user_agent("'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'", Some("en-US,en"), Some("Mac OS X")).unwrap();
+
+    tab.call_method(Page::AddScriptToEvaluateOnNewDocument {
+        source: include_str!("./stealth_scripts.js").to_string(),
+        world_name: None,
+        include_command_line_api: None,
+    })
+    .unwrap();
+}
+
+pub fn apply_css_rules(body: &headless_chrome::browser::tab::element::Element) {
+    let styles = include_str!("./styles.css");
+    let _ = body.call_js_fn(
+        &format!(
+            r#"
+            function apply_css() {{
+                function injectCSS(cssString) {{
+                    const style = document.createElement('style');
+                    style.type = 'text/css';
+                    if (style.styleSheet) {{
+                        style.styleSheet.cssText = cssString;
+                    }} else {{
+                        style.appendChild(document.createTextNode(cssString));
+                    }}
+                    document.head.appendChild(style);
+                }}
+                injectCSS(`{styles}`);
+            }}
+        "#
+        ),
+        vec![],
+        false,
+    );
+}
+
+pub fn test_launch() {
+    headless_chrome::Browser::new(
+        LaunchOptionsBuilder::default()
+            .sandbox(false)
+            .path(
+                std::env::var("CHROME_PATH")
+                    .ok()
+                    .map(|p| PathBuf::from_str(&p).unwrap()),
+            )
+            .build()
+            .unwrap(),
+    )
+    .expect("could not launch chromium");
+}
+
+pub fn fetch_page_with_chrome(url: &str) -> Option<ChromeResult> {
+    let opt_proxy = std::env::var("OPT_PROXY").unwrap_or_default();
+
+    let url_parsed = reqwest::Url::parse(url).unwrap();
+    let host = url_parsed.host_str().unwrap_or_default();
+
+    let mut r = 0;
+    let mut force_proxy = false;
+    loop {
+        r += 1;
+
+        if r >= 5 {
+            return None;
+        }
+
+        if r > 0 {
+            log::debug!("{host}: retrying");
+        }
+
+        r += 1;
+
+        let proxy = if force_proxy {
+            Some(opt_proxy.clone())
+        } else {
+            None
+        };
+
+        let browser = match headless_chrome::Browser::new(
+            LaunchOptionsBuilder::default()
+                .sandbox(false)
+                .proxy_server(proxy.as_deref())
+                .path(
+                    std::env::var("CHROME_PATH")
+                        .ok()
+                        .map(|p| PathBuf::from_str(&p).unwrap()),
+                )
+                .build()
+                .unwrap(),
+        ) {
+            Ok(b) => b,
+            Err(err) => {
+                log::error!("failed to start chrome: {}", err);
+                return None;
+            }
+        };
+
+        let tab = browser
+            .new_tab_with_options(CreateTarget {
+                url: url.to_string(),
+                width: Some(1366),
+                height: Some(768),
+                browser_context_id: None,
+                enable_begin_frame_control: None,
+                new_window: None,
+                background: None,
+            })
+            .unwrap();
+
+        stealth_tab(tab.clone());
+
+        tab.set_default_timeout(Duration::from_secs(15));
+
+        if let Err(err) = tab.navigate_to(url) {
+            log::error!("{host}: {}", err);
+            force_proxy = true;
+            continue;
+        }
+
+        if let Err(err) = tab.wait_until_navigated() {
+            log::error!("{host}: {}", err);
+            force_proxy = true;
+            continue;
+        }
+
+        log::debug!("{host}: sleeping for 2 seconds...");
+        sleep(Duration::from_secs(2));
+
+        // in case the page hasn't been fully loaded yet;
+        log::debug!("{host}: sleeping some more...");
+        let _ = tab.wait_for_element_with_custom_timeout("body * *", Duration::from_secs(10));
+
+        if let Ok(body) = tab.wait_for_element("body") {
+            apply_css_rules(&body);
+
+            // remove every element on page that has position: fixed property;
+            // this is to combat any potential popups and alerts
+            let _ = body.call_js_fn(
+                r#"
+                    function remove_fixed() {
+                        function clear() {
+                            // document.querySelectorAll("*").forEach(el =>  {
+                            //     if (el.computedStyleMap().get("position")?.value === "fixed") { 
+                            //         el.outerHTML = "";
+                            //     }
+                            // })
+                        }
+
+                        clear();
+
+                        setInterval(() => {
+                            clear()
+                        }, 5);
+                    }
+                "#,
+                vec![],
+                false,
+            );
+        }
+
+        let html = tab.get_content().unwrap();
+
+        let png_data = tab
+            .capture_screenshot(Page::CaptureScreenshotFormatOption::Png, None, None, true)
+            .unwrap();
+
+        let _ = tab.close(true);
+
+        return Some(ChromeResult {
+            screenshot: png_data,
+            html,
+        });
+    }
+}
